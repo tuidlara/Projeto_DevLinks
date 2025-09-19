cf050af0c547a44f2ed9a5aa112da7f0f9690216
Arthur de Lara <arthur.larazilli@hotmail.com>
2025-09-19 15:23:16 -0300

    tada: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function ToggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, ativar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  } //se tiver sem light mode, deixar imagem normal
+}
